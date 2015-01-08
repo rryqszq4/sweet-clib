031c5e98a10c734e1fccab5179e7a964a0db1a35
rryqszq4 <279664262@163.com>
2015-01-08 23:31:15 +0800

    [app_bns] 动态分配指针数组，拷贝各个切分单元到数组
    修改bug

--- lib/app/bns/weapon_handle.c
+++ lib/app/bns/weapon_handle.c
@@ -5,6 +5,26 @@
 #define LINE_BUF 8192
 #define TITLE_BUF 1024
 
+char *my_csv_2(char *s1, char *s2[],int m){
+	char **p;
+	p = s2;
+	char *enter = "\n";
+
+	while (*s1 != *enter && !isspace(*s1)){
+		*p[m] = *s1;
+		//printf("123\n");
+		p++;
+		s1++;
+	}
+
+	if (*s1 != *enter && isspace(*s1)){
+		s1++;
+	}
+
+	return s1;
+
+}
+
 char *my_csv(char *s1, char *s2){
 	char *p;
 	p = s2;
@@ -40,6 +60,7 @@ int main(int argc, char *argv[])
 	char tmp_title[TITLE_BUF+1];
 	char *title[300];
 	char *ss1;
+	char **tp;
 	int n,i;
 	char *enter = "\n";
 
@@ -69,14 +90,18 @@ int main(int argc, char *argv[])
 				memset(tmp_title,0,sizeof(char)*(TITLE_BUF+1));
 				sprintf(ss1, "%s", my_csv(ss1,tmp_title));
 				
-				title[n] = tmp_title;	
-				printf("title: %s:\n",tmp_title);
+				title[n] = (char*)malloc(sizeof(strlen(tmp_title))); // 动态分配数组指针
+				//title[n] = tmp_title;	// 拷贝指针地址
+				strcpy(title[n],tmp_title); // 拷贝指针中的具体内容 
+				//printf("title: %p\n",tmp_title);
+				//printf("title: %p:\n",title[n]);
 				n++;
 				//if (n > 300)
 				//	break;
 			}
 			for (i = 0; i< 300; i++){
 				printf("#%d %s\n",i,title[i]);
+				free(title[i]);
 			}
 		}
 		
