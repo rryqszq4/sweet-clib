4adfdc52afc488c83a2c80969c56a37c0f2c6b95
rryqszq4 <279664262@163.com>
2015-02-12 12:23:58 +0800

    [msgpack] add first

---
+++ lib/msgpack/first.c
@@ -0,0 +1,26 @@
+#include <msgpack.h>
+#include <stdio.h>
+
+int main(void)
+{
+	msgpack_sbuffer* buffer = msgpack_sbuffer_new();
+	msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
+
+	msgpack_pack_array(pk, 2);
+	msgpack_pack_raw(pk, 5);
+	msgpack_pack_raw_body(pk, "hello", 5);
+	msgpack_pack_raw(pk, 11);
+	msgpack_pack_raw_body(pk, "messagepack", 11);
+
+	msgpack_unpacked msg;
+	msgpack_unpacked_init(&msg);
+	bool success = msgpack_unpack_next(&msg, buffer->data, buffer->size, NULL);
+
+	msgpack_object obj = msg.data;
+	msgpack_object_print(stdout, obj);
+	printf("\n");
+
+	msgpack_sbuffer_free(buffer);
+	msgpack_packer_free(pk);
+
+}
