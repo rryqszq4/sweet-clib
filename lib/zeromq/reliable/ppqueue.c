6e71085e57ed380d40292695743cf905fb5bb88c
rryqszq4 <279664262@163.com>
2015-08-31 19:06:11 +0800

    [zmq] ppqueue udpate

--- lib/zeromq/reliable/ppqueue.c
+++ lib/zeromq/reliable/ppqueue.c
@@ -81,6 +81,70 @@ int main(void)
 	void *backend = zsocket_new(ctx, ZMQ_ROUTER);
 	zsocket_bind(frontend, "tcp://127.0.0.1:5555");
 	zsocket_bind(backend, "tcp://127.0.0.1:5556");
+
+	zlist_t *workers = zlist_new();
+
+	uint64_t heartbeat_at = zclock_time() + HEARTBEAT_INTERVAL;
+
+	while (true){
+		zmq_pollitem_t items [] ={
+			{backend, 0, ZMQ_POLLIN, 0},
+			{frontend, 0, ZMQ_POLLIN, 0}
+		};
+
+		int rc = zmq_poll(items, zlist_size(worker)?2:1);
+		if (rc == -1)
+			break;
+
+		if (items[0].revent & ZMQ_POLLIN){
+			zmsg_t *msg = zmsg_recv(backend);
+			if (!msg)
+				break;
+
+			zframe_t *identity = zmsg_unwrap(msg);
+			worker_t *worker = s_worker_new(identity);
+			s_worker_ready(worker, workers);
+
+			if (zmsg_size(msg) == 1){
+				zframe_t *frame = zmsg_first(msg);
+				if (memcmp(zframe_data(frame), PPP_READY, 1) && memcmp(zframe_data(frame), PPP_HEARTBEAT, 1)){
+					printf("E: invalid message from worker");
+					zmsg_dump(msg);
+				}
+				zmsg_destroy(&msg);
+			}
+			else
+				zmsg_send(&msg, frontend);
+		}
+		if (items[1].revent & ZMQ_POLLIN){
+			zmsg_t *msg = zmsg_recv(frontend);
+			if (!msg)
+				break;
+			zframe_t *identity = s_workers_next(workers);
+			zmsg_prepend(msg, &identity);
+			zmsg_send(&msg, backend);
+		}
+
+		if (zclock_time() >= heartbeat_at){
+			worker_t *worker = (worker_t *)zlist_first(workers);
+			while (worker){
+				zframe_send(&worker->identity, backend, ZFRAME_REUSE + ZFRAME_MORE);
+				zframe_t *frame = zframe_new(PPP_HEARTBEAT, 1);
+				zframe_send(&frame, backend, 0);
+				worker = (worker_t *)zlist_next(workers);
+			}
+			heartbeat_at = zclock_time() + HEARTBEAT_INTERVAL;
+		}
+	s_workers_purge(workers);
+	}
+
+	while (zlist_size(workers)){
+		worker_t *worker = (worker_t *)zlist_pop(workers);
+		s_worker_destroy(&worker);
+	}
+	zlist_destroy(&workers);
+	zctx_destroy(&ctx);
+	return 0;
 }
 
 
