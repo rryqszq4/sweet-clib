46a142bb09b8587c2c5cddba12861d9ecb89bb1f
rryqszq4 <279664262@163.com>
2015-09-02 18:43:08 +0800

    [zmq] md broker main

--- lib/zeromq/MDP/mdbroker.c
+++ lib/zeromq/MDP/mdbroker.c
@@ -328,6 +328,79 @@ s_worker_waiting(worker_t *self)
 	s_service_dispatch(self->service, NULL);
 }
 
+int main (int argc, char *argv[])
+{
+	int verbose = (argc > 1 && streq(argv[1], "-v"));
+
+	broker_t *self = s_broker_new(verbose);
+	s_broker_bind(self, "tcp://127.0.0.1:5555");
+
+	while(true){
+		zmq_pollitem_t items [] = {
+			{self->socket, 0, ZMQ_POLLIN, 0}
+		};
+		int rc = zmq_poll(items, 1, HEARTBEAT_INTERVAL * ZMQ_POLL_MSEC);
+		if (rc == -1)
+			break;
+
+		if (items[0].revents & ZMQ_POLLIN){
+			zmsg_t *msg = zmsg_recv(self->socket);
+			if (!msg)
+				break;
+			if (self->verbose){
+				zclock_log("I: received message");
+				zmsg_dump(msg);
+			}
+			zframe_t *sender = zmsg_pop(msg);
+			zframe_t *empty = zmsg_pop(msg);
+			zframe_t *header = zmsg_pop(msg);
+
+			if (zframe_streq(header, MDPC_CLIENT))
+				s_broker_client_msg(self, sender, msg);
+			else
+			if (zframe_streq(header, MDPW_WORKER))
+				s_broker_worker_msg(self, sender, msg);
+			else{
+				zclock_log("E: invalid message:");
+				zmsg_dump(msg);
+				zmsg_destroy(&msg);
+			}
+			zframe_destroy(&sender);
+			zframe_destroy(&empty);
+			zframe_destroy(&header);
+		}
+
+		if (zclock_time() > self->heartbeat_at){
+			s_broker_purge(self);
+			worker_t *worker = (worker_t *)zlist_first(self->waiting);
+			while (worker){
+				s_worker_send(worker, MDPW_HEARTBEAT, NULL, NULL);
+				worker = (worker_t *)zlist_next(self->waiting);
+			}
+			self->heartbeat_at = zclock_time() + HEARTBEAT_INTERVAL;
+		}
+	}
+
+	if (zctx_interrupted)
+		printf("W: interrupt receivde, shutting down...\n");
+
+	s_broker_destroy(&self);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
