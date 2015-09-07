b5d5ead47da417c8f145abaf710bc3c3f8d6698a
rryqszq4 <279664262@163.com>
2015-09-07 17:05:07 +0800

    [struct] 前序中序后序递归遍历

--- lib/_structs/traverse.c
+++ lib/_structs/traverse.c
@@ -2,6 +2,24 @@
 #include "bitree.h"
 #include "traverse.h"
 
+int preorder(const BiTreeNode *node, List *list)
+{
+	if (!bitree_is_eob(node)){
+		if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
+			return -1;
+
+		if (!bitree_is_eob(bitree_left(node)))
+			if (preorder(bitree_left(node), list) != 0)
+				return -1;
+
+		if (!bitree_is_eob(bitree_right(node)))
+			if (preorder(bitree_right(node), list) != 0)
+				return -1;
+	}
+
+	return 0;
+}
+
 int inorder(const BiTreeNode *node, List *list){
 	if (!bitree_is_eob(node)){
 
@@ -9,7 +27,7 @@ int inorder(const BiTreeNode *node, List *list){
 			if (inorder(bitree_lef(node), list) != 0)
 				return -1;
 
-		if (list_ins_nex(list, list_tail(list), bitree_data(node)) != 0)
+		if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
 			return -1;
 
 		if (!bitree_is_eob(bitree_right(node)))
@@ -19,3 +37,33 @@ int inorder(const BiTreeNode *node, List *list){
 
 	return 0;
 }
+
+int postorder(const BiTreeNode *node, List *list)
+{
+	if (!bitree_is_eob(node)){
+		if (!bitree_is_eob(bitree_left(node)))
+			if (postorder(bitree_left(node), list) != 0)
+				return -1;
+
+		if (!bitree_is_eob(bitree_right(node)))
+			if (postorder(bitree_right(node), list) != 0)
+				return -1;
+
+		if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0)
+			return -1;
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
