c7dd81cf3250d62d4c9af45f55b76db36377bb31
rryqszq4 <279664262@163.com>
2015-09-10 20:20:37 +0800

    [struct] bistro hide and lookup function

--- lib/_structs/bistree.c
+++ lib/_structs/bistree.c
@@ -257,6 +257,55 @@ insert(Bistree *tree, BiTreeNode **node, const void *data, int *balanced)
 	return 0;
 }
 
+static int
+hide(BisTree *tree, BiTreeNode *node, const void *data)
+{
+	int cmpval, retval;
+
+	if (bitree_is_eob(node)){
+		return -1;
+	}
+
+	cpmval = tree->compare(data, ((AvlNode *)bitree_data(node))->data);
+
+	if (cpmval < 0){
+		retval = hide(tree, bitree_left(node), data);
+	}else if (cmpval > 0){
+		retval = hide(tree, bitree_right(node), data);
+	}else {
+		((AvlNode *)bitree_data(node))->hidden = 1;
+		retval = 0;
+	}
+	return retval;
+}
+
+static int 
+lookup(BisTree *tree, BiTreeNode *node, void **data)
+{
+	int cmpval, retval;
+
+	if (bitree_is_eob(node)){
+		return -;
+	}
+
+	cmpval = tree->compare(*data, ((AvlNode *)bitree_data(node))->data);
+
+	if (cmpval < 0){
+		retval = lookup(tree, bitree_left(node), data);
+	}else if (cmpval > 0){
+		retval = lookup(tree, bitree_right(node), data);
+	}else {
+		if (!((AvlNode *)bitree_data(node))->hidden){
+			*data = ((AvlNode *)bitree_data(node))->data;
+			retval = 0;
+		}
+	}else {
+		return -1;
+	}
+
+	return retval;
+}
+
 
 
 
